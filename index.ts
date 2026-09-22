305401ceb1b3972dc83440d350012e7442746d1c
Tim Nelson <tbnelson@gmail.com>
2026-09-22 12:19:32 -0400

    thin-thread prototype initial

---
+++ index.ts
@@ -0,0 +1,54 @@
+/**
+ * Prototyping a throttling/rate-limiter extension for pi.dev, 
+ * specifically for use in CSCI 0320. By creating our own, we can 
+ * both avoid external dependencies and produce useful course material
+ * for when students are creating their own extensions.
+ */
+
+import type {
+  BeforeProviderRequestEvent,
+  ExtensionAPI,
+  MessageEndEvent,
+  MessageUpdateEvent,
+} from "@earendil-works/pi-coding-agent";
+
+/**
+ * Extensions export a single function that runs when the extension starts. 
+ * It registers a number of callbacks corresponding to pi.dev events.
+ *    "Handlers run in extension load and registration order." 
+ * @param pi a handle to pi's extensions library
+ */
+export default function (pi: ExtensionAPI) {
+  let loggedEarlyInput = false;
+
+  /**
+   * A request is about to be sent. Reset this extension's per-request state
+   */
+  pi.on("before_provider_request", (event: BeforeProviderRequestEvent) => {
+    loggedEarlyInput = false;
+    console.log("[throttle] -> request sent");
+  });
+
+  /**
+   * We might get more than one of these events if streaming responses are enabled.
+   * Since our goal is to detect the input-token count early, we don't want to 
+   * do anything here beyond the first update.
+   */
+  pi.on("message_update", (event: MessageUpdateEvent) => {
+    if (loggedEarlyInput || event.message.role !== "assistant") return;
+    const { input } = event.message.usage;
+    if (input > 0) {
+      loggedEarlyInput = true;
+      console.log(`[throttle] .. input known early: ${input} (stream event: ${event.assistantMessageEvent.type})`);
+    }
+  });
+
+  /**
+   * A message has ended, so we should know how many output tokens were really used.
+   */
+  pi.on("message_end", (event: MessageEndEvent) => {
+    if (event.message.role !== "assistant") return;
+    const { input, output, totalTokens } = event.message.usage;
+    console.log(`[throttle] <- final usage: input=${input} output=${output} total=${totalTokens}`);
+  });
+}
