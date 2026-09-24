a32c676fb487f6cd1dfdc9953d332333a9f5ef11
Tim Nelson <tbnelson@gmail.com>
2026-09-24 10:09:44 -0400

    increase ceiling again

--- defaults.ts
+++ defaults.ts
@@ -2,12 +2,12 @@ export const DEFAULT_CONFIG = {
   /** Window size. Using 1 minute, since it matches the TPM config unit. */
   windowMs: 60_000,
   /** Approximate max per-user tokens per minute. */
-  ceilingTokens: 250_000,
+  ceilingTokens: 300_000,
   /**
    * Prevent a very large request from stalling the session. It isn't clear
    * whether this should be done or, if it is, what the right value is.
    */
-  maxDelayMs: 30_000,
+  maxDelayMs: 20_000,
   /**
    * Don't delay sending requests for under this amount of time. Wait for
    * more debt to accumulate.
